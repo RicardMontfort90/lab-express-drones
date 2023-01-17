b57658c6748b4a5aad31cfb0a5500357e20086b5
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-01-17 16:43:31 +0100

    work in progress

--- routes/drones.js
+++ routes/drones.js
@@ -40,9 +40,16 @@ router.get('/drones/:id/edit', async (req, res, next) => {
   }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  const { id } = req.params;
+  try {
+    await Drone.findByIdAndUpdate(id, { name, propellers, maxSpeed });
+    res.redirect('/drones');
+  } catch (error) {
+    next(error);
+  } 
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
